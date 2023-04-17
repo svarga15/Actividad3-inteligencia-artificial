8ece6c37700a5f32b77b837949c40f1b0a2057d5
svarga15 <svarga15@ibero.edu.co>
2023-04-16 19:54:06 -0500

    subiendo actividad

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from sklearn import tree
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def aprensizaje():
+    # el primero valor corresponde a la altura, el segundo al peso, y el tercero a la temperatura
+    caracteristicas = [[8, 0.6, 50],
+                       [8, 0.6, 51],
+                       [8, 0.6, 52],
+                       [39, 0.8, 67],
+                       [39, 0.8, 68],
+                       [39, 0.8, 69]]
+
+    # 0 corresponde a gato y 1 a perro, segun las caracteristicas anteriores
+    valores = [0, 0, 0, 1, 1, 1]
+
+    # esta funcion es la que vamos a ulitizar
+    clasificaciones = tree.DecisionTreeClassifier()
+
+    # lo podemos a entrenar todos los patrones para que hagas las predicciones
+    clasificaciones.fit(caracteristicas, valores)
+
+    respuesta = clasificaciones.predict([[39, 0.8, 67]])
+
+    print(respuesta)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    aprensizaje()
